1ab03d91d37aebc805cbd5a37118c3cd8955da5b
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2023-02-13 20:09:20 +0600

    registration page done

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,38 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import './Registration.css';
 
 const Registration = () => {
+    const [signupError, setSignupError] = useState('');
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const handelSignUp = data => {
+        console.log(data);
+        setSignupError('');
+    }
+
     return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+        <div className='grid grid-cols-2 gap-0'>
+            <div className='bgOverlay flex justify-center items-center'>
+                <div className='text-left mx-10'>
+                    <p className='text-7xl font-bold mb-10'>Looks like you're new here!</p>
+                    <p className='text-3xl'>Join with us! Sign up with your details to get started!</p>
+                </div>
+            </div>
+            <div className='flex items-center justify-center'>
+                <div className='w-2/3'>
+                    <p className='text-4xl text-left mb-4 font-bold'>Sing Up</p>
+                    <form onSubmit={handleSubmit(handelSignUp)}>
+                        <div className="form-control w-full">
+                            <label className="label"><span className="label-text text-xl">First Name</span></label>
+                            <input placeholder='First Name' type="text"
+                                {...register("firstName",
+                                    { required: "First Name is required" })} className="input input-bordered w-full" />
+                            {errors.firstName && <p className='text-red-600'>{errors.firstName?.message}</p>}
+                        </div>
+                        <div className="form-control w-full">
+                            <label className="label"><span className="label-text text-xl">Last Name</span></label>
+                            <input placeholder='Last Name' type="text"
+                                {...register("lastName",
+                                    { required: "Last Name is required" })} className="input input-bordered w-full" />
+                            {errors.lastName && <p className='text-red-600'>{errors.lastName?.message}</p>}
+                        </div>
+                        <div className="form-control w-full">
+                            <label className="label"><span className="label-text text-xl">Email Address</span></label>
+                            <input placeholder='Email Address' type="email"
+                                {...register("email",
+                                    { required: "Email Address is required" })} className="input input-bordered w-full" />
+                            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                        </div>
+                        <div className="form-control w-full">
+                            <label className="label"><span className="label-text text-xl">Phone Number</span></label>
+                            <input placeholder='Phone Number' type="text"
+                                {...register("phone",
+                                    { required: "Phone Number is required" })} className="input input-bordered w-full" />
+                            {errors.phone && <p className='text-red-600'>{errors.phone?.message}</p>}
+                        </div>
+                        <div className="form-control w-full">
+                            <label className="label"><span className="label-text text-xl">Password</span></label>
+                            <input placeholder='Password' type="password"
+                                {...register("password",
+                                    {
+                                        required: "Password is required",
+                                        minLength: { value: 6, message: "Password must be 6 characters long" },
+                                        pattern: { value: /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: "Password must be strong" }
+                                    })} className="input input-bordered w-full" />
+                            {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                        </div>
+                
+                        <input className='btn bg-[#795548] hover:bg-[#512d1f] w-full mt-5' value='Signup' type="submit" />
+                        {
+                            signupError && <p className='text-red-600'>{signupError}</p>
+                        }
+                    </form>
+                    <p className='text-sm text-center'>Already have an account? <Link className='text-[#41281f] font-bold' to='/login'>Please Login</Link></p>
+                    <div className="divider">OR</div>
+                    <button 
+                    className='btn btn-outline hover:bg-[#795548] w-full '>
+                    CONTINUE WITH GOOGLE</button>
+                </div>
+
+            </div>
         </div>
     );
 };
